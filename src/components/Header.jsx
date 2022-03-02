14cf0522c06bb6906bd8f0a96d2bbcd80f9c42f4
Davina-dev <davinamr91@gmail.com>
2022-03-02 11:15:56 +0100

    add images

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,18 @@ import React from "react";
 const Header = () => {
   return (
     <div>
-      <h1>Header</h1>
+      <div><img src="/assets/img/unicorn-WM.png" title="Unicorn WM" alt="logo unicorn" /></div>
+      
+      <nav>
+       
+        <a href=""> GardenMap </a>
+        <a href=""> OurStory </a>
+        <a href=""> Village </a>
+        <a href=""> Comunity </a>
+        <p>|</p>
+        <a href="">Green Projects</a>
+      </nav>
+      <span><img src="/assets/img/menu.png" title="Unicorn WM" alt="logo unicorn" /></span>
     </div>
   );
 };

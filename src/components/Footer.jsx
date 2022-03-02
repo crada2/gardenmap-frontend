0caca8ceb79f91d107ccbf9510a3832eccceb0d2
MayuRuru <aruizrull@protonmail.com>
2022-03-02 11:47:56 +0100

    m

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,11 @@ import "../assets/styles/footer.css";
 const Footer = () => {
   return (
     <div className="footer">
-      <img className="logo" src={"./assets/img/logo.png"} alt="logo" />
+      <img
+        className="logo"
+        src={require("../assets/img/logo.png")}
+        alt="logo"
+      />
       <p>Copyright reserved to CRADA</p>
     </div>
   );

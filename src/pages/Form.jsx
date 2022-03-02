5c3e1854ae3d3aa1acb3fa41da6f729a79da1c08
MayuRuru <aruizrull@protonmail.com>
2022-03-02 12:48:59 +0100

    m

--- src/pages/Form.jsx
+++ src/pages/Form.jsx
@@ -1,10 +1,32 @@
 import React from "react";
+import Footer from "../components/Footer";
+import "../assets/styles/form.css";
 
-const Form = () => {
+export default function Form() {
   return (
-    <div>
-      <h1>FORM</h1>
-    </div>
+    <>
+      <div className="container">
+        <div className="containter-title">
+          <h3>FORMULARY</h3>
+        </div>
+
+        <div className="containter-content">
+          <fieldset>
+            <form className="content">
+              <label htmlFor="name">Name</label>
+              <input type="text" placeholder="What is your name?" />
+              <label htmlFor="surname">Surame</label>
+              <input type="text" placeholder="And your last name?" />
+              <label htmlFor="location">Location</label>
+              <input type="text" placeholder="Address of your garden" />
+              <label htmlFor="services">Offers</label>
+              <input type="text" placeholder="What do you want to offer?" />
+              <input type="submit" value="Submit" />
+            </form>
+          </fieldset>
+        </div>
+      </div>
+      <Footer />
+    </>
   );
-};
-export default Form;
+}

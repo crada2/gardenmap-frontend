aacc8324baf80af2463ce25714de2c3e47f1a0b5
MayuRuru <aruizrull@protonmail.com>
2022-03-03 12:39:56 +0100

    newform

--- src/components/GardenForm.jsx
+++ src/components/GardenForm.jsx
@@ -8,24 +8,48 @@ const GardenForm = () => {
         <div className="container-content">
           <form className="content">
             <div className="row">
-              <label htmlFor="name">Name</label>
-              <input type="text" placeholder="What is your name?" />
+              <div className="col-25">
+                <label htmlFor="name">Name</label>
+              </div>
+              <div className="col-75">
+                <input type="text" placeholder="What is your name?" />
+              </div>
             </div>
             <div className="row">
-              <label htmlFor="surname">Surname</label>
-              <input type="text" placeholder="And your last name?" />
+              <div className="col-25">
+                <label htmlFor="surname">Surname</label>
+              </div>
+              <div className="col-75">
+                <input type="text" placeholder="And your last name?" />
+              </div>
             </div>
             <div className="row">
-              <label htmlFor="location">Location</label>
-              <input type="text" placeholder="Where is your garden?" />
+              <div className="col-25">
+                <label htmlFor="location">Location</label>
+              </div>
+              <div className="col-75">
+                <input type="text" placeholder="Where is your garden?" />
+              </div>
             </div>
             <div className="row">
-              <label htmlFor="email">Email</label>
-              <input type="text" placeholder="What is your e-mail??" />
+              <div className="col-25">
+                <label htmlFor="email">Email</label>
+              </div>
+              <div className="col-75">
+                <input type="text" placeholder="What is your e-mail??" />
+              </div>
             </div>
             <div className="row">
-              <label htmlFor="services">Offers</label>
-              <input type="text" placeholder="What do you want to offer?" />
+              <div className="col-25">
+                <label htmlFor="services">Services</label>
+              </div>
+              <div className="col-75">
+                <select id="services" name="services">
+                  <option value="Rent">Rent my garden</option>
+                  <option value="Visit">Organize visits</option>
+                  <option value="Produce">Sell my produce</option>
+                </select>
+              </div>
             </div>
           </form>
           <button>Join the garden family!</button>

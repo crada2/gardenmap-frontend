632bb6f90ff163c3ed5659682163b50ececb5715
MayuRuru <aruizrull@protonmail.com>
2022-03-03 11:51:02 +0100

    go back

--- src/components/GardenForm.jsx
+++ src/components/GardenForm.jsx
@@ -1,33 +1,35 @@
 const GardenForm = () => {
   return (
-    <div className="container">
-      <div className="container-title">
-        <legend>Tell us about you</legend>
-      </div>
-      <div className="container-content">
-        <form className="content">
-          <div className="row">
-            <label htmlFor="name">Name</label>
-            <input type="text" placeholder="What is your name?" />
-          </div>
-          <div className="row">
-            <label htmlFor="surname">Surname</label>
-            <input type="text" placeholder="And your last name?" />
-          </div>
-          <div className="row">
-            <label htmlFor="location">Location</label>
-            <input type="text" placeholder="Where is your garden?" />
-          </div>
-          <div className="row">
-            <label htmlFor="email">Email</label>
-            <input type="text" placeholder="What is your e-mail??" />
-          </div>
-          <div className="row">
-            <label htmlFor="services">Offers</label>
-            <input type="text" placeholder="What do you want to offer?" />
-          </div>
-        </form>
-        <button>Join the garden family!</button>
+    <div className="background">
+      <div className="container">
+        <div className="container-title">
+          <legend>Tell us about you</legend>
+        </div>
+        <div className="container-content">
+          <form className="content">
+            <div className="row">
+              <label htmlFor="name">Name</label>
+              <input type="text" placeholder="What is your name?" />
+            </div>
+            <div className="row">
+              <label htmlFor="surname">Surname</label>
+              <input type="text" placeholder="And your last name?" />
+            </div>
+            <div className="row">
+              <label htmlFor="location">Location</label>
+              <input type="text" placeholder="Where is your garden?" />
+            </div>
+            <div className="row">
+              <label htmlFor="email">Email</label>
+              <input type="text" placeholder="What is your e-mail??" />
+            </div>
+            <div className="row">
+              <label htmlFor="services">Offers</label>
+              <input type="text" placeholder="What do you want to offer?" />
+            </div>
+          </form>
+          <button>Join the garden family!</button>
+        </div>
       </div>
     </div>
   );
--- src/pages/Form.jsx
+++ src/pages/Form.jsx
@@ -2,9 +2,5 @@ import "../assets/styles/form.css";
 import GardenForm from "../components/GardenForm";
 
 export default function Form() {
-  return (
-    <div className="background">
-      <GardenForm />
-    </div>
-  );
+  return <GardenForm />;
 }

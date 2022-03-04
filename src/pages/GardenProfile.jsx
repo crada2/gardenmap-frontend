8f284c25701aa0b3596aa96325a0d1ff33e1448b
Ceci <carbel.cecilia@gmail.com>
2022-03-04 09:22:40 +0100

    mainn profile

--- src/pages/GardenProfile.jsx
+++ src/pages/GardenProfile.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import Hero from "../components/Hero/Hero";
+import MainProfile from "../components/MainProfile";
 
 const GardenProfile = () => {
   return (
     <div>
-      <h1>Profile</h1>
+      <Hero/>
+      <MainProfile/>
     </div>
   );
 };

a8cc69d731f32c7ca7f16d68184f7bf0bebade62
MayuRuru <aruizrull@protonmail.com>
2022-03-11 09:48:08 +0100

    button create

--- src/pages/GardenProfile.jsx
+++ src/pages/GardenProfile.jsx
@@ -2,8 +2,9 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Hero from "../components/Hero/Hero";
 import MainProfile from "../components/MainProfile";
-import ServicesGarden from "./ServicesGarden";
+import ServicesGarden from "../components/ServicesGarden";
 import "../assets/styles/gardenProfile.css";
+import { Link } from "react-router-dom";
 
 const GardenProfile = () => {
   const [loading, setLoading] = useState(true);
@@ -15,11 +16,11 @@ const GardenProfile = () => {
       setProfile(res.data[0].products);
     });
   }, [setLoading]);
-  
+
   const handleDeleteGarden = (id) => {
-      const updatedGarden = profile.filter((profiles) => profiles.id !== id);
-      setProfile(updatedGarden);
-    };
+    const updatedGarden = profile.filter((profiles) => profiles.id !== id);
+    setProfile(updatedGarden);
+  };
 
   const renderProfile = () => {
     if (loading) {
@@ -33,29 +34,26 @@ const GardenProfile = () => {
         title={title}
         price={price}
         onDelete={() => handleDeleteGarden(id)}
-       
-        />
-      ));
-    };
+      />
+    ));
+  };
 
-    return (
+  return (
     <div>
-     
       <Hero />
       <div className="profileUserHero">
-      <MainProfile/> 
-      <iframe
+        <MainProfile />
+        <Link to="/form">
+          <button className="card__btn-create">Create </button>
+        </Link>
+        <iframe
           className="ifraMapProfile"
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3172.3325395304414!2d-122.01116148467422!3d37.33463524513264!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x808fb59127ce078f%3A0x18e1c3ce7becf1b!2sApple%20Park!5e0!3m2!1sen!2sin!4v1637309850935!5m2!1sen!2sin"
         ></iframe>
       </div>
-      
-
       <div className="grid-container">{renderProfile()}</div>
-     
     </div>
-      );
-    };
+  );
+};
 
-    
 export default GardenProfile;

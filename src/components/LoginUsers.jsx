b74f2666880d332c8ec76c6090e44db05d884886
Davina-dev <davinamr91@gmail.com>
2022-03-22 14:18:24 +0100

    add bg login

--- src/components/LoginUsers.jsx
+++ src/components/LoginUsers.jsx
@@ -8,9 +8,18 @@ const LoginUsers = () => {
  
        <div className="login_container">
 
-          <div>
+          <div className="login_card">
           <h1>Login to GardenMaps</h1>
-          <section className="login_card"> cardForm</section>
+
+          <form action="" method="post">
+             <label htmlFor="">Name: <input type="text" /></label>
+            </form>
+            <form action="" method="post">
+             <label htmlFor="">Password: <input type="text" /></label>
+            </form>
+             
+         
+          <img src="https://media4.giphy.com/media/iy7h2RteA2Et5fBSYN/200w.webp?cid=ecf05e47chujau85xi8so3qte1nbc4yi9p59ahqpmc86bdrf&rid=200w.webp&ct=s" alt="" />
             
           </div>
                 

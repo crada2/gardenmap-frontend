cb543829f41a5d4586be22b809262fb33399b919
Davina-dev <davinamr91@gmail.com>
2022-03-23 11:44:51 +0100

    profile garden

--- src/components/MainProfile.jsx
+++ src/components/MainProfile.jsx
@@ -13,6 +13,7 @@ const MainProfile = ({ owner }) => {
           <p className="garden_profile-info"> 666666666 {owner.telephone}</p>
           <p className="garden_profile-info"> pedro@pedro.com{owner.email}</p>
           <p className="garden_profile-info">calle pamplona 6, casa{owner.direction}</p>
+          <button className="garden_create-service"> Greate my service</button>
         </div>
         <div className="garden_map_box">
        <iframe className="garden_iframe"  frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=720&amp;height=600&amp;hl=es&amp;q=Les%20Rambles,%201%20Barcelona,%20Spain+(GardenMaps)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe>

05122ff4cabcf9187a1edf1142ab50c0425f61ce
Ceci <carbel.cecilia@gmail.com>
2022-03-28 05:04:20 +0200

    adding changes navbar, hero, arrow, readme

--- src/components/Hero/BtnSlider.jsx
+++ src/components/Hero/BtnSlider.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable jsx-a11y/alt-text */
 import React from "react";
-import leftArrow from "../../assets/img/left-arrow.svg";
-import rightArrow from "../../assets/img/right-arrow.svg";
+import leftArrow from "../../assets/img/arrow-lefth.svg";
+import rightArrow from "../../assets/img/arrow-right.svg";
 import "../../assets/styles/hero.css";
 
 export default function BtnSlider({ direction, moveSlide }) {
